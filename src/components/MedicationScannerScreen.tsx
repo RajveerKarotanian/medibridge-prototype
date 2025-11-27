f7e6db3a6bb7f4c97335e539659642aa0e5d39e7
RajveerK <rajveerkarotanian06@gmail.com>
2025-11-26 20:55:44 -0500

    Fix button position

--- src/components/MedicationScannerScreen.tsx
+++ src/components/MedicationScannerScreen.tsx
@@ -173,12 +173,12 @@ export function MedicationScannerScreen({ onNavigate }: MedicationScannerScreenP
               </div>
             </div>
 
-            {/* Shutter button - Fixed to bottom center */}
+            {/* Shutter button - iOS Style Positioning */}
             <button
               onClick={handleCapture}
-              className="fixed bottom-8 left-1/2 -translate-x-1/2 z-50 w-20 h-20 rounded-full bg-white border-4 border-gray-300 hover:scale-105 transition-transform shadow-lg"
+              className="absolute bottom-10 left-1/2 -translate-x-1/2 z-50 w-20 h-20 rounded-full border-4 border-white flex items-center justify-center transition-transform active:scale-95"
             >
-              <div className="w-full h-full rounded-full bg-white"></div>
+              <div className="w-16 h-16 rounded-full bg-white"></div>
             </button>
           </div>
         )}
